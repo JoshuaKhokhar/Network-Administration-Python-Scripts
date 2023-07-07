4b25fa8bd368cd1d74ab349b452bc38d979d3a3a
JoshuaKhokhar <131722135+JoshuaKhokhar@users.noreply.github.com>
2023-07-07 13:07:12 -0400

    Add files via upload

---
+++ verifyInternetConnectivity.py
@@ -0,0 +1,22 @@
+"""
+Script Name: verifyInternetConnectivity.py
+Author: Joshua Khokhar
+Creation Date: 7/7/2023
+Purpose: This script verifies the internet connectivity of the host device.
+"""
+
+import requests
+import socket
+
+testUrl = "https://www.youtube.com"
+timedOutWait = 5
+hostname = socket.gethostname()
+
+
+for i in range(5):
+    try:
+        request = requests.get(testUrl, timeout=timedOutWait)
+        print(f"{hostname} is connected to the Internet.")
+    except:
+        print(f"{hostname} is not connected to the Internet.")
+
